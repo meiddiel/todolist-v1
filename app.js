e91f865ec5cb206cb3b23c7730215a6720749c9f
Meiddiel <tokymickaelrivo@gmail.com>
2023-08-06 19:28:27 +0300

    introdiuce Ejs

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ const app = express();
 const port = 3000;
 
 app.get('/', function(req, res){
-  res.send('Hello');
+  
+  var today = new Date();
+  var currentDay = today.getDay();
+
+  if (currentDay === 6 || currentDay === 0){
+    res.write('<h1>Yay it is the weekend!</h1)');
+  }else{
+    res.sendFile(__dirname + '/index.html');
+  }
 });
 
 app.listen(port, function(){
