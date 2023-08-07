b530a2d611771f407c39dbe6099d55bf063ae02b
Meiddiel <tokymickaelrivo@gmail.com>
2023-08-07 13:25:43 +0300

    Passing data through the server to Temple

--- app.js
+++ app.js
@@ -5,42 +5,36 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
+var items = ['buy food', 'Cook food', 'Eat Food'];
 
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
 
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = '';
+  var option = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
+  };
 
-  switch(currentDay){
-    case 0:
-      day = 'Sunday';
-      break;
-    case 1:
-      day = 'Monday';
-      break;
-    case 2:
-      day = 'Tuesday';
-      break;
-    case 3:
-      day = 'Wednesdy';
-      break;
-    case 4:
-      day = 'Thursday';
-      break;
-    case 5:
-      day = 'Friday';
-      break;
-    case 6:
-      day = 'Saturday';
-      break;
-    default:
-      console.log('Error: Current day is equal to : ' + currentDay);
-  }
+var day = today.toLocaleDateString('en-US', option);
 
-  res.render('list', {kindOfDay: day});
+  res.render('list', {
+    kindOfDay: day,
+    newListItems: items
+  });
+
+});
+
+app.post('/', function(req, res){
+  var item = req.body.newItem
+
+  items.push(item);
+
+  res.redirect('/');
 
 });
 
