a3ddcbf73a30705e5c7745e3ab62708f6c9bf02f
Meiddiel <tokymickaelrivo@gmail.com>
2023-08-07 15:58:30 +0300

    Adding Pre-Made CSs

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ let items = ['buy food', 'Cook food', 'Eat Food'];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
 
